48d260bd273cd18f81ce12e1f1aa6bd512472252
DejaunG <dejaun.gayle@outlook.com>
2023-11-21 00:28:28 -0500

    added a the ability for the user to input their own values

--- src/windowClass.java
+++ src/windowClass.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class windowClass {
 
     private double baseRatePerWindow;
@@ -38,21 +40,77 @@ public class windowClass {
     }
 
     public static void main(String[] args) {
-        windowClass estimator = new windowClass(
-                7.0, // Base rate per window in New Jersey
-                3.0, // Extra charge for hard-to-reach windows
-                2.5, // Extra charge for complex windows
-                4.0, // Extra charge for heavily soiled windows
-                600.0, // Overhead costs in New Jersey
-                15.0); // Profit margin percentage
+
+        System.out.println("Would you like to input your own rates into the calculator? (y/n)");
+        Scanner userInput = new Scanner(System.in);
+        String userChoice = userInput.next();
+
+        if (userChoice.equals("y"))
+        {
+            System.out.println("Please enter your base rate per window: ");
+            double baseRate = userInput.nextDouble();
+            System.out.println("Please enter your extra charge for hard-to-reach windows: ");
+            double hardToReachExtra = userInput.nextDouble();
+            System.out.println("Please enter your extra charge for complex windows: ");
+            double complexExtra = userInput.nextDouble();
+            System.out.println("Please enter your extra charge for heavily soiled windows: ");
+            double soiledExtra = userInput.nextDouble();
+            System.out.println("Please enter your overhead costs: ");
+            double overhead = userInput.nextDouble();
+            System.out.println("Please enter your profit margin percentage: ");
+            double profitMargin = userInput.nextDouble();
+
+            windowClass selfEstimator = new windowClass(
+                    baseRate, // Base rate per window in New Jersey
+                    hardToReachExtra, // Extra charge for hard-to-reach windows
+                    complexExtra, // Extra charge for complex windows
+                    soiledExtra, // Extra charge for heavily soiled windows
+                    overhead, // Overhead costs in New Jersey
+                    profitMargin); // Profit margin percentage
+
+            System.out.println("Please enter the number of windows you would like to clean: ");
+            int numWindows = userInput.nextInt();
+            System.out.println("Please enter the number of hard-to-reach windows you would like to clean: ");
+            int numHardToReach = userInput.nextInt();
+            System.out.println("Please enter the number of complex windows you would like to clean: ");
+            int numComplex = userInput.nextInt();
+            System.out.println("Please enter the number of heavily soiled windows you would like to clean: ");
+            int numSoiled = userInput.nextInt();
+
+            double totalCost = selfEstimator.calculateTotalCost(
+                    numWindows, // Total number of windows
+                    numHardToReach,  // Number of hard-to-reach windows
+                    numComplex,  // Number of complex windows
+                    numSoiled); // Number of heavily soiled windows
+
+            System.out.println("Total cost for cleaning " + numWindows + " windows: $" + totalCost);
 
 
-        double totalCost = estimator.calculateTotalCost(
-                1000, // Total number of windows
-                100,  // Number of hard-to-reach windows
-                150,  // Number of complex windows
-                200); // Number of heavily soiled windows
 
-        System.out.println("Total cost for cleaning 1000 windows: $" + totalCost);
+        }
+
+        else if (userChoice.equals("n")){
+
+            windowClass estimator = new windowClass(
+                    7.0, // Base rate per window in New Jersey
+                    3.0, // Extra charge for hard-to-reach windows
+                    2.5, // Extra charge for complex windows
+                    4.0, // Extra charge for heavily soiled windows
+                    600.0, // Overhead costs in New Jersey
+                    15.0); // Profit margin percentage
+
+
+            double totalCost = estimator.calculateTotalCost(
+                    1000, // Total number of windows
+                    100,  // Number of hard-to-reach windows
+                    150,  // Number of complex windows
+                    200); // Number of heavily soiled windows
+
+            System.out.println("Total cost for cleaning 1000 windows: $" + totalCost);
+       }
+        else {
+            System.out.println("Please enter a valid input.");
+        }
+
     }
 }
